08e3c28596c96ca407f90298c77ad94b79514d46
soufiiyane <soufiyane.aitmoulay@gmail.com>
2024-08-09 00:00:14 +0100

    pre-sign-up function

--- src/initializeDatabase.py
+++ src/initializeDatabase.py
@@ -13,9 +13,10 @@ def lambda_handler(event, context):
         """
         CREATE TABLE User (
             userId INT AUTO_INCREMENT PRIMARY KEY,
-            name VARCHAR(255) NOT NULL,
+            firstName VARCHAR(255) NOT NULL,
+            lastName VARCHAR(255) NOT NULL,
+            userName VARCHAR(255) NOT NULL,
             email VARCHAR(255) NOT NULL UNIQUE,
-            password VARCHAR(255) NOT NULL,
             role VARCHAR(50) NOT NULL,
             createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
             updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
